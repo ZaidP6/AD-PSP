e6c2b5cabc75fc260ceecf5cc029a920a229cc71
ZaidP6 <pilarads06@gmail.com>
2024-12-11 09:40:33 +0100

    proyecto demo / estructura

---
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/producto/123")
+    public Producto getProducto(){
+        return new Producto(123L, "un producto chulo");
+    }
+
+    record Producto(Long id, String nombre){}
+
+}
