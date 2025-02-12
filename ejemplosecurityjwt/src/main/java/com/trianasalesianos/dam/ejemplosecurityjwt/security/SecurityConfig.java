b8e4de564670ea5bd755c795cb9d22fb43009fad
ZaidP6 <pilarads06@gmail.com>
2025-02-12 13:21:49 +0100

    properties modificada

--- ejemplosecurityjwt/src/main/java/com/trianasalesianos/dam/ejemplosecurityjwt/security/SecurityConfig.java
+++ ejemplosecurityjwt/src/main/java/com/trianasalesianos/dam/ejemplosecurityjwt/security/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
 
         http
                 .cors(Customizer.withDefaults())
-                .csrf().disable()
+                .csrf(csrf -> csrf.disable())
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler)
